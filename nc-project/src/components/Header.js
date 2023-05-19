3be9a589f47690b463bd94a2d8df675d59c32de1
Heather Greenberg <hgreenberg1017@gmail.com>
2023-05-18 20:55:04 -0700

    add header file

---
+++ nc-project/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Header () {
+  return (
+    <header>
+      <div className="header">
+      <img id="headerlogo" src="./public/nclogo1.jpg" alt="Header Logo"/>
+       
+        <div className="header">
+          <a className="active" href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#contact">Contact</a>
+          <a href="#reviews">Reviews</a>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
