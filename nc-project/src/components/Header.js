903fed7600f69e7bc676c1c45de55d04c69654f6
George S <reneehe@George-Sem-2.local>
2023-05-22 20:59:44 -0700

    remove hash from header home link

--- nc-project/src/components/Header.js
+++ nc-project/src/components/Header.js
@@ -7,7 +7,7 @@ function Header () {
       <img id="headerlogo" src="./public/nclogo1.jpg" alt="Header Logo"/>
        
         <div className="header">
-          <a className="active" href="#home">Home</a>
+          <a className="active" href="home">Home</a>
           <a href="about">About</a>
           <a href="contact">Contact</a>
           <a href="reviews">Reviews</a>
