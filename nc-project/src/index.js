ef990ec9ecbd09b56bbddf9fd71ffeddd5493869
George S <reneehe@George-Sem-2.local>
2023-05-22 21:23:28 -0700

    remove comments

--- nc-project/src/index.js
+++ nc-project/src/index.js
@@ -7,10 +7,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
-  <ChakraProvider>
+<ChakraProvider>
   <App />
 </ChakraProvider>,
 );
--- nc-project/src/pages/Contact.js
+++ nc-project/src/pages/Contact.js
@@ -11,34 +11,6 @@ import {
 
 function ContactForm() {
   return (
-    // <form>
-    // <FormControl id="firstName" isRequired borderColor="red.500" borderWidth={2}>
-    //     <FormLabel>First Name</FormLabel>
-    //     <Input type="text" />
-    //   </FormControl>
-
-    //   <FormControl id="lastName" isRequired borderColor="red.500" borderWidth={2}>
-    //     <FormLabel>Last Name</FormLabel>
-    //     <Input type="text" />
-    //   </FormControl>
-
-    //   <FormControl id="phoneNumber" isRequired borderColor="red.500" borderWidth={2}>
-    //     <FormLabel>Phone Number</FormLabel>
-    //     <Input type="tel" />
-    //   </FormControl>
-
-    //   <FormControl id="email" isRequired borderColor="red.500" borderWidth={2}> 
-    //     <FormLabel>Email</FormLabel>
-    //     <Input type="email" />
-    //   </FormControl>
-
-    //   <FormControl id="message" isRequired borderColor="red.500" borderWidth={2}>
-    //     <FormLabel>Message</FormLabel>
-    //     <Textarea />
-    //   </FormControl>
-
-    //   <Button variant="red" type="submit">Submit</Button>
-    // </form>
     <Flex>
     <Box flex={1}>
       <form>
