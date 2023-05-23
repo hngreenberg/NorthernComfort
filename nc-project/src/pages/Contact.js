47efe879c95da3379fd0a0a0a16401ee950900c5
George S <reneehe@George-Sem-2.local>
2023-05-22 21:15:01 -0700

    update link in header and update index.js to include chackra wrapper, also update contact form with chakra

--- nc-project/src/pages/Contact.js
+++ nc-project/src/pages/Contact.js
@@ -1,27 +1,81 @@
 import React from 'react';
-import { Button } from "@chakra-ui/react";
+import {   
+    FormControl,
+    FormLabel,
+    Input,
+    Textarea,
+    Button,
+    Flex,
+    Box,
+} from "@chakra-ui/react";
 
 function ContactForm() {
   return (
-    <form>
-    
-      <label htmlFor="firstName">First Name:</label>
-      <input type="text" id="firstName" name="firstName" required />
+    // <form>
+    // <FormControl id="firstName" isRequired borderColor="red.500" borderWidth={2}>
+    //     <FormLabel>First Name</FormLabel>
+    //     <Input type="text" />
+    //   </FormControl>
 
-      <label htmlFor="lastName">Last Name:</label>
-      <input type="text" id="lastName" name="lastName" required />
+    //   <FormControl id="lastName" isRequired borderColor="red.500" borderWidth={2}>
+    //     <FormLabel>Last Name</FormLabel>
+    //     <Input type="text" />
+    //   </FormControl>
 
-      <label htmlFor="phoneNumber">Phone Number:</label>
-      <input type="tel" id="phoneNumber" name="phoneNumber" required />
+    //   <FormControl id="phoneNumber" isRequired borderColor="red.500" borderWidth={2}>
+    //     <FormLabel>Phone Number</FormLabel>
+    //     <Input type="tel" />
+    //   </FormControl>
 
-      <label htmlFor="email">Email:</label>
-      <input type="email" id="email" name="email" required />
+    //   <FormControl id="email" isRequired borderColor="red.500" borderWidth={2}> 
+    //     <FormLabel>Email</FormLabel>
+    //     <Input type="email" />
+    //   </FormControl>
 
-      <label htmlFor="message">Message:</label>
-      <textarea id="message" name="message" required></textarea>
+    //   <FormControl id="message" isRequired borderColor="red.500" borderWidth={2}>
+    //     <FormLabel>Message</FormLabel>
+    //     <Textarea />
+    //   </FormControl>
 
-      <Button variant="red" type="submit">Submit</Button>
-    </form>
+    //   <Button variant="red" type="submit">Submit</Button>
+    // </form>
+    <Flex>
+    <Box flex={1}>
+      <form>
+        <FormControl id="firstName" isRequired>
+          <FormLabel>First Name</FormLabel>
+          <Input type="text" />
+        </FormControl>
+
+        <FormControl id="lastName" isRequired>
+          <FormLabel>Last Name</FormLabel>
+          <Input type="text" />
+        </FormControl>
+
+        <FormControl id="phoneNumber" isRequired>
+          <FormLabel>Phone Number</FormLabel>
+          <Input type="tel" />
+        </FormControl>
+
+        <FormControl id="email" isRequired>
+          <FormLabel>Email</FormLabel>
+          <Input type="email" />
+        </FormControl>
+
+        <FormControl id="message" isRequired>
+          <FormLabel>Message</FormLabel>
+          <Textarea />
+        </FormControl>
+
+        <Button variant="red" type="submit">
+          Submit
+        </Button>
+      </form>
+    </Box>
+    <Box flex={1} bg="blue" color="white" p={4}>
+    <p>test</p>
+    </Box>
+  </Flex>
   );
 }
 
