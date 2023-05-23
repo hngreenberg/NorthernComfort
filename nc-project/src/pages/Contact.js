3ae0566f6f58d642bdf82d8dca1ff2f4edd8fa5a
George S <reneehe@George-Sem-2.local>
2023-05-22 21:35:39 -0700

    cleanup contact hours style a little bit

--- nc-project/src/pages/Contact.js
+++ nc-project/src/pages/Contact.js
@@ -44,25 +44,25 @@ function ContactForm() {
         </Button>
       </form>
     </Box>
-    <Box flex={1} bg="blue" color="white" p={4} display="flex" alignItems="center" justifyContent="center">
-        <div>
-            <h2>NORTHERN COMFORT LLC</h2>
-            <p>Arlington, WA 98223</p>
+    <Box flex={1} bg="blue" color="white" p={4} display="flex" alignItems="center" justifyContent="center" fontSize="20px">
+    <div>
+        <h2 class="contactHeader" mb={4} fontWeight="600">NORTHERN COMFORT LLC</h2>
+        <p>Arlington, WA 98223</p>
 
-            <h3>CALL US</h3>
-            <p>Phone: (425) 588-4139</p>
+        <h3 class="contactHeader" mb={2} fontWeight="600">CALL US</h3>
+        <p fontWeight="600">Phone: <a href="tel:+14255884139" style={{ color: "red" }}>(425) 588-4139</a></p>
 
-            <h3>HOURS</h3>
-            <p>Mon: 8:00AM-8:00PM</p>
-            <p>Tue: 8:00AM-8:00PM</p>
-            <p>Wed: 8:00AM-8:00PM</p>
-            <p>Thu: 8:00AM-8:00PM</p>
-            <p>Fri: 8:00AM-8:00PM</p>
-            <p>Sat: 8:00AM-8:00PM</p>
-            <p>Sun: Closed</p>
+        <h3 class="contactHeader" mb={2} fontWeight="600">HOURS</h3>
+        <p>Mon: 8:00AM-8:00PM</p>
+        <p>Tue: 8:00AM-8:00PM</p>
+        <p>Wed: 8:00AM-8:00PM</p>
+        <p>Thu: 8:00AM-8:00PM</p>
+        <p>Fri: 8:00AM-8:00PM</p>
+        <p>Sat: 8:00AM-8:00PM</p>
+        <p>Sun: Closed</p>
 
-            <p>24 Hour Emergency Services Available</p>
-        </div>
+        <p>24 Hour Emergency Services Available</p>
+    </div>
     </Box>
   </Flex>
   );
