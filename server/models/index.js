f92c575a03a645837ef2511253f17b6401d00d47
George S <reneehe@George-Sem-2.local>
2023-05-24 19:38:34 -0700

    fix index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,6 @@
 const Tech = require('./Tech');
 const Matchup = require('./Matchup');
-const Reviewmodel = require('./Reviewmodel');
 const Admin = require('./admin');
 const ContactMessage = require('./ContactMessage');
 
-module.exports = { Tech, Matchup, Reviewmodel, Admin, ContactMessage };
+module.exports = { Tech, Matchup, Admin, ContactMessage };
