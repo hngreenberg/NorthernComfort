e72ac851fc52940cfbf44b31cfcbc7ef4d124933
Heather Greenberg <hgreenberg1017@gmail.com>
2023-05-24 20:19:37 -0700

    homepage fonts

--- nc-project/src/components/Header.js
+++ nc-project/src/components/Header.js
@@ -5,7 +5,14 @@ function Header() {
     <header>
       <div className="header">
         <img id="headerlogo" src="nclogo1.jpg" alt="" />  
-        <h2 className="headtitle">YOUR COMFORT MATTERS TO US!</h2>
+        <h2 className="headtitle" style={{
+                fontWeight: "bold",
+                color: "blue",
+                textAlign: "center",
+                fontSize: "4em",
+                fontFamily: "'Satisfy', cursive",
+                textShadow: "3px 5px 4px black",
+              }}>YOUR COMFORT MATTERS TO US! </h2>
       </div>
     </header>
   );
