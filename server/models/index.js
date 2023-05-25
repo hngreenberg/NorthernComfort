410304063d8553f28fa9b2188968aee5196799ea
George S <reneehe@George-Sem-2.local>
2023-05-24 19:26:26 -0700

    add admin and contact form message models, update index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,6 @@
 const Tech = require('./Tech');
 const Matchup = require('./Matchup');
+const Reviewmodel = require('./Reviewmodel');
+const Admin = require('./Admin');
 
 module.exports = { Tech, Matchup };
