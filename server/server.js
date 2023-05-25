f36a7025a5d72f65791fe3caea33cd4ab16fea28
George S <reneehe@George-Sem-2.local>
2023-05-24 20:34:38 -0700

    update admin model and server.js

--- server/server.js
+++ server/server.js
@@ -1,30 +1,36 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-
-const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+// const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
+require('dotenv').config();
 
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
+  console.log('in production');
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.get('/', (req, res) => {
+  let paththingy = path.join(__dirname, '../client/build/index.html');
+  console.log(paththingy);
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-
-// Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
   await server.start();
   server.applyMiddleware({ app });
@@ -37,6 +43,5 @@ const startApolloServer = async () => {
   })
   };
   
-// Call the async function to start the server
+
   startApolloServer();
- 
