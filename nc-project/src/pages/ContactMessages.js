f978169856d80d994be7b8402b1afa59dbf160e7
George S <reneehe@George-Sem-2.local>
2023-05-24 20:08:15 -0700

    add messages page and queries

---
+++ nc-project/src/pages/ContactMessages.js
@@ -0,0 +1,136 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Container,
+  Card,
+  Button,
+  Row,
+  Col
+} from 'react-bootstrap';
+
+import { useQuery, useMutation } from '@apollo/client';
+
+// import { getMe, deleteBook } from '../utils/API';
+import { GET_ME } from '../utils/queries';
+
+import Auth from '../utils/auth';
+import { removeBookId } from '../utils/localStorage';
+import { LOGIN_ADMIN, REMOVE_BOOK } from '../utils/mutations';
+import { ContactMessage } from '../../../server/models';
+
+const ContactMessages = () => {
+  // const [userData, setUserData] = useState({});
+  const {loading, data } = useQuery(LOGIN_ADMIN);
+
+  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
+
+  // use this to determine if `useEffect()` hook needs to run again
+  // const userDataLength = Object.keys(userData).length;
+  // console.log(data)
+
+const userData = data?.me || {};
+  // useEffect(() => {
+  //   const getUserData = async () => {
+  //     try {
+  //       const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+  //       if (!token) {
+  //         return false;
+  //       }
+
+  //       const response = await getMe(token);
+
+  //       if (!response.ok) {
+  //         throw new Error('something went wrong!');
+  //       }
+
+  //       const user = await response.json();
+  //       setUserData(user);
+  //     } catch (err) {
+  //       console.error(err);
+  //     }
+  //   };
+
+  //   getUserData();
+  // }, [userDataLength]);
+
+
+
+  // create function that accepts the book's mongo _id value as param and deletes the book from the database
+  const handleDeleteBook = async (bookId) => {
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    // const { data } = useMutation(REMOVE_BOOK);
+
+    if (!token) {
+      return false;
+    }
+
+    try {
+      // const response = await deleteBook(bookId, token);
+      // const { data } = await saveBook({
+      //   variables: { input:bookToSave},
+      // });
+
+      // if book successfully saves to user's account, save book id to state
+      // setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+
+      const {data} = await removeBook({
+        variables: { bookId },
+      });
+
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
+
+      // const updatedUser = await response.json();
+      // setUserData(updatedUser);
+      // // upon success, remove book's id from localStorage
+      removeBookId(bookId);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // if data isn't here yet, say so
+  if (loading) {
+    return <h2>LOADING...</h2>;
+  }
+
+  return (
+    <>
+      <div className='text-light bg-dark p-5'>
+        <Container>
+          <h1>Viewing saved books!</h1>
+        </Container>
+      </div>
+      <Container>
+        <h2 className='pt-5'>
+          {userData.savedBooks.length
+            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
+            : 'You have no saved books!'}
+        </h2>
+        <Row>
+          {userData.savedBooks.map((book, index) => {
+            return (
+              <Col md="4" key={index}>
+                <Card key={book.bookId} border='dark'>
+                  {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+                  <Card.Body>
+                    <Card.Title>{book.title}</Card.Title>
+                    <p className='small'>Authors: {book.authors}</p>
+                    <Card.Text>{book.description}</Card.Text>
+                    <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                      Delete this Book!
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default ContactMessages;
