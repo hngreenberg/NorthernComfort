be2586777d2cc6fd2bff94f064263512854e54c7
George S <reneehe@George-Sem-2.local>
2023-05-24 20:28:00 -0700

    update resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,27 +1,46 @@
-const { Tech, Matchup, Reviewmodel } = require('../models');
+const { Tech, Matchup, Reviewmodel, Admin, ContactMessage } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
+  // Query: {
+  //   tech: async () => {
+  //     return Tech.find({});
+  //   },
+  //   matchups: async (parent, { _id }) => {
+  //     const params = _id ? { _id } : {};
+  //     return Matchup.find(params);
+  //   },
+  // },
   Query: {
-    tech: async () => {
-      return Tech.find({});
-    },
-    matchups: async (parent, { _id }) => {
-      const params = _id ? { _id } : {};
-      return Matchup.find(params);
+    admin: async (parent, args, context) => {
+      if (context.user) {
+        const user = await User.findById(context.user._id).populate('savedBooks');
+        return user;
+      }
+      throw new Error('You need to be logged in!');
     },
   },
   Mutation: {
-    createMatchup: async (parent, args) => {
-      const matchup = await Matchup.create(args);
-      return matchup;
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new Error("Can't find this user");
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new Error('Wrong password!');
+      }
+      const token = signToken(user);
+      return { token, user };
     },
-    createVote: async (parent, { _id, techNum }) => {
-      const vote = await Matchup.findOneAndUpdate(
-        { _id },
-        { $inc: { [`tech${techNum}_votes`]: 1 } },
-        { new: true }
-      );
-      return vote;
+    addContactMessage: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      if (!user) {
+        throw new Error('Something went wrong!');
+      }
+      const token = signToken(user);
+      return { token, user };
     },
   },
 };
