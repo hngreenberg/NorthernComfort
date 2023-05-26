bc9bef91ac96a43d60e649fba0c9252cdbc558e0
George S <reneehe@George-Sem-2.local>
2023-05-25 18:30:22 -0700

    update admin login styling

--- nc-project/src/pages/AdminLogin.js
+++ nc-project/src/pages/AdminLogin.js
@@ -51,42 +51,51 @@ const LoginForm = () => {
   };
   return (
     <>
+    <div style={{ display: 'flex', justifyContent: 'center', border: '2px solid blue', borderRadius: '3%', padding: '20px', marginRight: '35%', marginLeft: '35%'}}>
       <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
         <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
           Something went wrong with your login credentials!
         </Alert>
-        <Form.Group className='mb-3'>
-          <Form.Label htmlFor='email'>Email</Form.Label>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+        {/* <Form.Group className='mb-3'> */}
+          <Form.Label htmlFor='email' style={{ fontWeight: 'bold', color: 'blue' }}>Email:</Form.Label>
           <Form.Control
             type='text'
-            placeholder='Your email'
+            placeholder='Enter your email'
             name='email'
             onChange={handleInputChange}
             value={userFormData.email}
             required
+            style={{ borderColor: 'blue', fontWeight: 'bold', color: 'blue' }}
           />
-          <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
-        </Form.Group>
+          {/* <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback> */}
+        {/* </Form.Group> */}
+        </div>
 
-        <Form.Group className='mb-3'>
-          <Form.Label htmlFor='password'>Password</Form.Label>
+        {/* <Form.Group className='mb-3'> */}
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <Form.Label htmlFor='password' style={{ fontWeight: 'bold', color: 'blue' }}>Password:</Form.Label>
           <Form.Control
             type='password'
-            placeholder='Your password'
+            placeholder='Enter your password'
             name='password'
             onChange={handleInputChange}
             value={userFormData.password}
             required
+            style={{ borderColor: 'blue', fontWeight: 'bold', color: 'blue' , marginBottom: '10px'}}
           />
-          <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
-        </Form.Group>
+          {/* <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback> */}
+        {/* </Form.Group> */}
+        </div>
         <Button
           disabled={!(userFormData.email && userFormData.password)}
           type='submit'
-          variant='success'>
-          Submit
+          variant='success'
+          style={{ backgroundColor: 'blue', border: '2px solid blue', borderRadius: '2%', color: 'white', fontWeight: 'bold' }}>
+          Login
         </Button>
       </Form>
+      </div>
     </>
   );
 };
