e4140b0672869cb0717112d361f98dded481788b
George S <reneehe@George-Sem-2.local>
2023-05-25 20:18:10 -0700

    update footer

--- nc-project/src/components/Footer.js
+++ nc-project/src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className="footer" style={{ backgroundColor: 'lightblue', marginTop: '10px', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
       <img id="footerlogo" src="mascot.png" alt="" style={{ marginBottom: '10px' }} />
-      <p style={{ color: 'blue' }}>© 2023 CALL NOW! (415)5800-001</p>
+      <p style={{ color: 'blue' }}>© 2023 CALL NOW! (415) 580-0001</p>
     </div>
   );
 }
